c3b289d1ad29e729297132ddf2d6e01c4de376a9
Garrett Moody <garrettkmoody@gmail.com>
2025-10-07 00:17:45 -0700

    firebase deploy

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb2cTiiYF6MTP-cVhdHz7NjYMYVY-FmLE",
+  authDomain: "appsupportpages.firebaseapp.com",
+  projectId: "appsupportpages",
+  storageBucket: "appsupportpages.firebasestorage.app",
+  messagingSenderId: "811638621673",
+  appId: "1:811638621673:web:d654a8b165706241627778"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
